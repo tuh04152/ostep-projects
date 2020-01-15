79a0f49a420a3226924d60ec38feab9b4f12d8e4
tuh04152 <59936397+tuh04152@users.noreply.github.com>
2020-01-15 16:20:40 -0500

    Initial adding of the wcat file. Includes three test txt files.
    The three test files, wcat.c, and the gcc'ed files are all uploaded at this time.

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
-int main(){
+#include <stdlib.h>
+int main(int argc, char** argv){
+    if (argc == 1) {
+        return 0;
+    }
+    for (int i = 1; i < argc; i++){
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL){
+            printf("wcat: cannot open file\n");
+            exit(1);
+        }
+        char buffer[256];
+        while (fgets(buffer, 256, fp) != NULL){
+        printf("%s", buffer);
+        }
+        fclose(fp);
+        printf("\n\n");
+        }
 
+    return 0;
 }
