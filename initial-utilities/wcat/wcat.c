fb68c2018d8278fd0fd3edbd2d29ee65b211df49
tuh04152 <59936397+tuh04152@users.noreply.github.com>
2020-01-22 00:48:40 -0500

    Updated wcat with comments

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char** argv){
+    // If the only argument is the program name, exit the program immediately.
     if (argc == 1) {
         return 0;
     }
+    // Otherwise for every argument after the program name, we open the files using the arguments as filenames
+    printf("\n");
     for (int i = 1; i < argc; i++){
         FILE *fp = fopen(argv[i], "r");
         if (fp == NULL){
             printf("wcat: cannot open file\n");
             exit(1);
         }
-        char buffer[256];
-        while (fgets(buffer, 256, fp) != NULL){
+        // Next we define a buffer and default size. The getline will function, of course, realloc this buffer if it is too small for the line input.
+        char* buffer;
+        size_t number = 256;
+        buffer = (char*)malloc(256 * sizeof(char));
+        if (buffer == NULL){
+            printf("Memory Error.");
+            exit(1);
+        }
+        // Every line in the file is printed (as getline returns -1 at EOF)
+        while (getline(&buffer, &number, fp) != -1){
         printf("%s", buffer);
         }
+        // Finally, we close the file and print two newline characters to separate all the files.
         fclose(fp);
         printf("\n\n");
         }
